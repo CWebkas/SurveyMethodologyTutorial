5dc6136371b4ad1fb2048af141f0ca70918747da
niklashaffert <50962410+niklashaffert@users.noreply.github.com>
2021-05-31 13:15:37 +0200

    update sampling session

--- Code_Data/SessionData/usa_2016/create_us_2016.R
+++ Code_Data/SessionData/usa_2016/create_us_2016.R
@@ -1,28 +1,68 @@
-library(dplyr)
-library(tidyr)
+# Packages ----------------------------------------------------------------
 
-# data source: https://dataverse.harvard.edu/dataset.xhtml;jsessionid=1dd02598ea6d8fb7c74571ac4772?persistentId=doi%3A10.7910%2FDVN%2FVOQCHQ&version=&q=&fileTypeGroupFacet=%22Tabular+Data%22&fileAccess=&fileTag=&fileSortField=&fileSortOrder=
+suppressWarnings(if (!require("pacman"))
+  install.packages("pacman", repos = "http://cran.r-project.org"))
 
-load("SessionData/usa2016/countypres_2000-2016.RData")
+pacman::p_load(data.table, tidyverse)
+
+
+# Data --------------------------------------------------------------------
+
+# source:
+# https://dataverse.harvard.edu/dataset.xhtml;jsessionid=1dd02598ea6d8fb7c74571ac4772?persistentId=doi%3A10.7910%2FDVN%2FVOQCHQ&version=&q=&fileTypeGroupFacet=%22Tabular+Data%22&fileAccess=&fileTag=&fileSortField=&fileSortOrder=
+
+## load data
+#load("SessionData/usa_2016/countypres_2000-2016.RData")
+load("../SessionData/usa_2016/countypres_2000-2016.RData")
+
+
+# Processing --------------------------------------------------------------
 
 df_wide <-
   x %>%
   as_tibble() %>%
+  # filter down to the year we are interested in (2016)
   filter(year == 2016) %>%
+  # select three variables (state, candidates name and candidates votes) we are interested in
   select(state, candidate, candidatevotes) %>%
+  # remove NAs
   drop_na() %>%
+  # summarize candidate votes by state as they are on county level
   group_by(state, candidate) %>%
   summarise(candidatevotes = sum(candidatevotes)) %>%
   ungroup() %>%
+  # create one column for each candidate
   pivot_wider(values_from = candidatevotes,
               names_from = candidate) %>%
-  rename(trump = 2,
-         clinton = 3,
-         other = 4)
+  # create readable column names
+  rename(Trump = 2,
+         Clinton = 3,
+         Other = 4) %>%
+  mutate(state = as.character(state))
 
 df_long <-
   df_wide %>%
+  # bring wide data set into long format (one observation per voter)
   pivot_longer(!state) %>%
-  uncount(value)
+  uncount(value) %>%
+  # add unique id for each voter
+  mutate(voter_id = 1:nrow(.)) %>%
+  rename(vote = name)
 
-saveRDS(df_long, "SessionData/usa_2016/us_2016.rds")
+## get turnout by state
+turnout_state <-
+  df_wide %>%
+  # grouping by state as we want the turnout on the state level
+  group_by(state) %>%
+  # summarize all votes for the turnout
+  summarise(turnout_state = sum(Trump, Clinton, Other)) %>%
+  ungroup()
+
+# merge the two data frames
+df_final <-
+  df_long %>%
+  left_join(turnout_state)
+
+# save data in rds format
+#fwrite(df_final, "SessionData/usa_2016/us_2016.gz")
+fwrite(df_final, "../SessionData/usa_2016/us_2016.gz")
